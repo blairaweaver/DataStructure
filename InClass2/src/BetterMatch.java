ae6143b51133457af3a91425b1e5fbfd804c4ace
blairweaver <bw09135@georgiasouthern.edu>
2018-09-06 10:51:34 -0400

    Added the classes that have notes on his solutions. Should put the code in as well at some point

---
+++ InClass2/src/BetterMatch.java
@@ -0,0 +1,3 @@
+public class BetterMatch {
+//    see photo
+}
---
+++ InClass2/src/BetterStack.java
@@ -0,0 +1,5 @@
+public class BetterStack {
+    DoubleLink stack = new DoubleLink();
+
+//    implement the methods here which call the necessary methods in DoubleLink
+}
