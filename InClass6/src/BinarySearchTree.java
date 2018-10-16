ec15c524d0d7465d8da0ac166b82ee0c796ca490
blairweaver <bw09135@georgiasouthern.edu>
2018-10-16 12:05:20 -0400

    Finished the implementation of the Binary Search Tree. The code could use some work to make it better or removal of code that isn't used

--- InClass6/src/BinarySearchTree.java
+++ InClass6/src/BinarySearchTree.java
@@ -19,6 +19,14 @@ public class BinarySearchTree<K extends Comparable<K>, E> {
 
         public K getKey() {return key;}
 
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
         public BinaryTreeNode<K, E> getParent() {
             return parent;
         }
@@ -43,18 +51,6 @@ public class BinarySearchTree<K extends Comparable<K>, E> {
             this.rightChild = r;
         }
 
-        public BinaryTreeNode<K, E> getSibling(BinaryTreeNode<K, E> sibling){
-            BinaryTreeNode<K, E> parent = sibling.getParent();
-            if (parent.rightChild == sibling) return parent.leftChild;
-            else return parent.rightChild;
-        }
-
-        public int numChildren() {
-            int sum = 0;
-            if (leftChild != null) sum++;
-            if (rightChild != null) sum++;
-            return sum;
-        }
     }
 
     private BinaryTreeNode<K, E> root;
@@ -65,6 +61,8 @@ public class BinarySearchTree<K extends Comparable<K>, E> {
     }
     public BinarySearchTree(K k, E e){
         this.root = new BinaryTreeNode<>(k, e, null);
+        root.setLeftChild(new BinaryTreeNode<>(null, null, root));
+        root.setRightChild(new BinaryTreeNode<>(null, null, root));
         size++;
     }
 
@@ -77,14 +75,6 @@ public class BinarySearchTree<K extends Comparable<K>, E> {
     public boolean isEmpty() {
         return this.size == 0;
     }
-
-//    reworked the isExternal
-//    public boolean isInternal(BinaryTreeNode<K, E> e){
-//        if (e.getLeftChild() != null && e.getRightChild() != null) {
-//            return true;
-//        }
-//        else return false;
-//    }
 
     public boolean isExternal(BinaryTreeNode<K, E> e){
         if (e.getElement() == null) {
@@ -99,12 +89,12 @@ public class BinarySearchTree<K extends Comparable<K>, E> {
 
     public void inOrder(BinaryTreeNode<K, E> e) {
         if (e.getLeftChild() != null) inOrder(e.getLeftChild());
-        System.out.print(e.getElement());
+        if (e.getElement() != null) System.out.print(e.getElement());
         if (e.getRightChild() != null) inOrder(e.getRightChild());
     }
 
     public void preOrder(BinaryTreeNode<K, E> e) {
-        System.out.print(e.getElement());
+        if (e.getElement() != null) System.out.print(e.getElement());
         if (e.getLeftChild() != null) preOrder(e.getLeftChild());
         if (e.getRightChild() != null) preOrder(e.getRightChild());
     }
@@ -112,28 +102,8 @@ public class BinarySearchTree<K extends Comparable<K>, E> {
     public void postOrder(BinaryTreeNode<K, E> e) {
         if (e.getLeftChild() != null) postOrder(e.getLeftChild());
         if (e.getRightChild() != null) postOrder(e.getRightChild());
-        System.out.print(e.getElement());
+        if (e.getElement() != null) System.out.print(e.getElement());
     }
-
-    public void addLayer(BinaryTreeNode<K, E> parent, E left, E right) {
-        BinaryTreeNode leftChild = new BinaryTreeNode<>(left, parent);
-        BinaryTreeNode rightChild = new BinaryTreeNode<>(right, parent);
-        parent.setLeftChild(leftChild);
-        size++;
-        parent.setRightChild(rightChild);
-        size++;
-        if (getHeightAtNode(leftChild) > height) {
-            height = getHeightAtNode(leftChild);
-        }
-    }
-
-    public int getHeightAtNode(BinaryTreeNode<K, E> e){
-//        root returns zero since the first call will add to the total
-        if (e.getParent() == null) return 0;
-        else return 1 + getHeightAtNode(e.getParent());
-    }
-
-    public int getHeight() {return height;}
 
     public BinaryTreeNode<K, E> TreeSearch(K k, BinaryTreeNode<K, E> e) {
         if (isExternal(e)) return e;
@@ -143,20 +113,119 @@ public class BinarySearchTree<K extends Comparable<K>, E> {
         else return TreeSearch(k, e.getRightChild());
     }
 
+    public void insert(K k, E e) {
+        BinaryTreeNode<K, E> search = TreeSearch(k, root);
+        if (isExternal(search)) add(search, k, e);
+        else updateNode(search, e);
+    }
+
+    public void delete(K k) {
+        BinaryTreeNode<K, E> search = TreeSearch(k, root);
+
+//        didn't find the key in tree
+        if (isExternal(search)) return;
+
+        BinaryTreeNode<K, E> parent = search.getParent();
+
+        int dir = 0;
+//      determine if the node to delete is left or right child of parent
+        if (parent.getLeftChild() == search) {
+            dir = -1;
+        }
+
+//        If the node only has null children
+        if (isExternal(search.getLeftChild()) && isExternal(search.getRightChild())) {
+            if (dir == -1) {
+                parent.setLeftChild(null);
+            }
+            else parent.setRightChild(null);
+            return;
+        }
+
+//        If the node has one null child
+        if (isExternal(search.getLeftChild())) {
+            if (dir == -1) {
+                parent.setLeftChild(search.getRightChild());
+                search.getRightChild().setParent(parent);
+            }
+            else {
+                parent.setRightChild(search.getRightChild());
+                search.getRightChild().setParent(parent);
+            }
+            return;
+        }
+
+//        If two children, find smallest on right side to place in old node spot
+        BinaryTreeNode<K, E> minRight = findMin(search.getRightChild());
+//        Remove link from parent of smallest
+        if (minRight.getParent().getLeftChild() == minRight) {
+            minRight.getParent().setLeftChild(new BinaryTreeNode<>(null, null, minRight.getParent()));
+        }
+        else {
+            minRight.getParent().setRightChild(new BinaryTreeNode<>(null, null, minRight.getParent()));
+        }
+//        add new link for smallest and other nodes
+        minRight.setParent(parent);
+        minRight.setRightChild(search.getRightChild());
+        minRight.setLeftChild(search.getLeftChild());
+        search.getRightChild().setParent(minRight);
+        search.getLeftChild().setParent(minRight);
+        if (dir == -1) {
+            parent.setLeftChild(minRight);
+        }
+        else {
+            parent.setRightChild(minRight);
+        }
+    }
+
+    private void add(BinaryTreeNode<K, E> old, K k, E e) {
+        old.setKey(k);
+        old.setElement(e);
+        old.setLeftChild(new BinaryTreeNode<>(null, null, old));
+        old.setRightChild(new BinaryTreeNode<>(null, null, old));
+    }
+
+    private void updateNode(BinaryTreeNode<K, E> old, E e) {
+        old.setElement(e);
+    }
+
+    private BinaryTreeNode<K, E> findMax(BinaryTreeNode<K, E> start) {
+        if (!isExternal(start.getRightChild())) return findMax(start.getRightChild());
+        else return start;
+    }
+
+    private BinaryTreeNode<K, E> findMin(BinaryTreeNode<K, E> start) {
+        if (!isExternal(start.getLeftChild())) return findMin(start.getLeftChild());
+        else return start;
+    }
+
     public static void main(String[] args){
-//        BinarySearchTree test = new BinarySearchTree<>('+');
-//        System.out.println(test.isEmpty());
-//        test.addLayer(test.getRoot(), '-', '3');
-//        test.addLayer(test.getRoot().getLeftChild(), '1', '3');
-//        System.out.println(test.getSize());
-//        System.out.println(test.isExternal(test.getRoot()));
-//        System.out.println(test.getHeight());
-//        System.out.println(test.isRoot(test.getRoot().getRightChild()));
-//        System.out.println(test.isInternal(test.getRoot()));
-//        test.inOrder(test.getRoot());
-//        System.out.println("");
-//        test.preOrder(test.getRoot());
-//        System.out.println("");
-//        test.postOrder(test.getRoot());
+        BinarySearchTree test = new BinarySearchTree(6, '6');
+        test.insert(2, '2');
+        test.insert(1, '1');
+        test.insert(4, '4');
+        test.insert(9, '9');
+        test.insert(8, '8');
+        System.out.println(test.TreeSearch(4, test.root).getKey());
+        test.inOrder(test.root);
+        test.insert(5, '5');
+        System.out.println();
+        test.inOrder(test.root);
+        test.delete(4);
+        System.out.println();
+        test.inOrder(test.root);
+
+        BinarySearchTree test2 = new BinarySearchTree(1, '1');
+        test2.insert(3, '3');
+        test2.insert(2, '2');
+        test2.insert(8, '8');
+        test2.insert(9, '9');
+        test2.insert(6, '6');
+        test2.insert(5, '5');
+        System.out.println();
+        test2.inOrder(test2.root);
+        test2.delete(3);
+        System.out.println();
+        test2.inOrder(test2.root);
     }
 }
