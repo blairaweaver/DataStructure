ce75789109cf096742d69353cd92615c1fc67533
blairweaver <bw09135@georgiasouthern.edu>
2018-11-08 13:08:21 -0500

    Started IC10. Almost done with all methods pertaining to Adj List and Adj Matrix. Haven't started on an implementation the Edge list

---
+++ InClass10/src/Graphs.java
@@ -0,0 +1,278 @@
+//Need to check all methods for the Vector[] for the case that there is not elements in the Array
+
+public class Graphs {
+    private class VectorNode<E extends Comparable<E>> {
+        private E element;
+        private VectorNode next;
+        private VectorNode previous;
+
+        public VectorNode(E e) {
+            this.element = e;
+            next = null;
+            previous = null;
+        }
+
+        public VectorNode(E e, VectorNode previous) {
+            this.element = e;
+            this.previous = previous;
+            this.next = null;
+        }
+
+        public VectorNode(E e, VectorNode previous, VectorNode next) {
+            this.element = e;
+            this.previous = previous;
+            this.next = next;
+        }
+
+        public void setElement(E element) {
+            this.element = element;
+        }
+
+        public E getElement() {
+            return element;
+        }
+
+        public VectorNode getNext() {
+            return next;
+        }
+
+        public VectorNode getPrevious() {
+            return previous;
+        }
+
+        public void setNext(VectorNode next) {
+            this.next = next;
+        }
+
+        public void setPrevious(VectorNode previous) {
+            this.previous = previous;
+        }
+
+        public boolean hasNext() {
+            return this.next != null;
+        }
+    }
+
+    private class EdgeNode {
+        private VectorNode origin;
+        private VectorNode dest;
+        private EdgeNode previous;
+        private EdgeNode next;
+
+        public EdgeNode() {
+            origin = null;
+            dest = null;
+            previous = null;
+            next = null;
+        }
+
+        public EdgeNode(VectorNode origin, VectorNode dest, EdgeNode previous, EdgeNode next) {
+            this.origin = origin;
+            this.dest = dest;
+            this.previous = previous;
+            this.next = next;
+        }
+
+        public VectorNode getOrigin() {
+            return origin;
+        }
+
+        public VectorNode getDest() {
+            return dest;
+        }
+
+        public EdgeNode getPrevious() {
+            return previous;
+        }
+
+        public EdgeNode getNext() {
+            return next;
+        }
+
+        public void setOrigin(VectorNode origin) {
+            this.origin = origin;
+        }
+
+        public void setDest(VectorNode dest) {
+            this.dest = dest;
+        }
+
+        public void setPrevious(EdgeNode previous) {
+            this.previous = previous;
+        }
+
+        public void setNext(EdgeNode next) {
+            this.next = next;
+        }
+    }
+
+    private int[][] adjMatrix;
+    private VectorNode[] adjList;
+    private int verticies = 0;
+    private int edges = 0;
+
+    public Graphs(int verticies) {
+        adjMatrix = new int[verticies][verticies];
+        adjList = new VectorNode[verticies];
+    }
+
+    public int[][] getAdjMatrix() {
+        return adjMatrix;
+    }
+
+    public VectorNode[] getAdjList() {
+        return adjList;
+    }
+
+    public int numVertices() {
+        return verticies;
+    }
+
+    public int numEdges() {
+        return edges;
+    }
+
+    public int outDegree(int[][] adjMatrix, int vert) {
+        int sum = 0;
+        for (int i = 0; i < adjMatrix[vert].length; i++) {
+            if (adjMatrix[vert][i] != -1) {
+                sum += adjMatrix[vert][i];
+            }
+        }
+        return sum;
+    }
+
+    public int outDegree(VectorNode[] adjList, int vert) {
+        int sum = 0;
+        VectorNode current = adjList[vert];
+        if (current != null) {
+            sum++;
+        }
+        while (current.hasNext()) {
+            current = current.getNext();
+            sum++;
+        }
+        return sum;
+    }
+
+//    Currently Assuming non directional graph
+//    public int inDegree() {}
+
+    public getEdge() {}
+
+    public getVerticies() {}
+
+    public void insertVertex() {
+        int oldSize = adjMatrix[0].length;
+        int[][] tempMatrix = new int[oldSize + 1][oldSize + 1];
+        VectorNode[] tempArray = new VectorNode[oldSize + 1];
+        for (int i = 0; i < adjMatrix.length; i++) {
+            tempMatrix[i][i] = adjMatrix[i][i];
+            tempArray[i] = adjList[i];
+        }
+        verticies++;
+    }
+
+    public void insertEdge(int vert1, int vert2) {
+        adjMatrix[vert1][vert2] = 1;
+        adjMatrix[vert2][vert1] = 1;
+        if (!edgeExist(vert1,vert2)) {
+            addEdgeToList(vert1,vert2);
+        }
+        edges++;
+    }
+
+    public void removeEdge(int vert1, int vert2) {
+        adjMatrix[vert1][vert2] = 0;
+        adjMatrix[vert2][vert1] = 0;
+        if (edgeExist(vert1,vert2)) {
+            VectorNode current = adjList[vert1];
+            remove(vert2, current);
+            current = adjList[vert2];
+            remove(vert1, current);
+        }
+        edges--;
+    }
+
+    public void removeVertex(int vert) {
+        for (int i = 0; i < adjMatrix.length; i++) {
+            adjMatrix[vert][i] = -1;
+            adjMatrix[i][vert] = -1;
+            if (i != vert && edgeExist(i,vert)) {
+                VectorNode current = adjList[i];
+                remove(vert, current);
+            }
+        }
+        adjList[vert] = null;
+        verticies--;
+    }
+
+    public boolean edgeExist(int vert1, int vert2) {
+        VectorNode current = adjList[vert1];
+        while (current.hasNext()) {
+            int cmp = current.getElement().compareTo(vert2);
+            if (cmp == 0) {
+                return true;
+            }
+            else {
+                current = current.getNext();
+            }
+        }
+
+        return false;
+    }
+
+    private void addEdgeToList(int vert1, int vert2) {
+        VectorNode current = adjList[vert1];
+        while (current.hasNext()) {
+            current = current.getNext();
+        }
+        current.setNext(new VectorNode(vert2, current, null));
+        current = adjList[vert2];
+        while (current.hasNext()) {
+            current = current.getNext();
+        }
+        current.setNext(new VectorNode(vert1, current, null));
+    }
+
+    private void remove(int vert1, VectorNode current) {
+        while (current.hasNext()) {
+            int cmp = current.getElement().compareTo(vert1);
+            if (cmp == 0) {
+                current.getPrevious().setNext(current.getNext());
+                current.getNext().setPrevious(current.getPrevious());
+            }
+            else {
+                current = current.getNext();
+            }
+        }
+    }
+
+    public void print(){
+        for (int i = 0; i < adjMatrix.length; i++) {
+            System.out.print(i + ": ");
+            for (int j = 0; j < adjMatrix.length; j++) {
+                if (adjMatrix[i][j] == 1) {
+                    System.out.print(j + " ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < adjMatrix.length; i++) {
+            System.out.print(i + ": ");
+            VectorNode current = adjList[i];
+            while (current != null && current.hasNext()) {
+                System.out.print(current.getElement() + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Graphs test = new Graphs(5);
+
+    }
+}
